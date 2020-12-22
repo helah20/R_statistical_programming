3a1acaf684d1a62d3b31f3e5b99fb642190759b5
hela <48882360+helah20@users.noreply.github.com>
2020-12-22 16:41:51 +0300

    Create LP_Brewery .R

---
+++ WEEK3/LP_Brewery .R
@@ -0,0 +1,41 @@
+library(lpSolve)
+
+?lp
+
+# Objective function coefficients
+obj_coeff <- c(13,23,30)
+
+# Constraint coefficients
+constraints <- matrix(c(5,15,10,
+                        4,4,4,
+                        35,20,15,
+                        5,10,20), nrow = 4, byrow = TRUE)
+
+# Direction of logical operators
+direction <- c("<=", 
+               "<=",
+               "<=",
+               "<="
+              )
+
+# Resources (Right hand side constraints)
+resources <- c(480,
+               160,
+               1190,
+               800)
+
+# Create the lp
+sol<-lp(
+  "max", 
+  obj_coeff, 
+  constraints, 
+  direction, 
+  resources)
+
+# Max Profit
+sol$objval
+
+# Best values for objective function
+ sol$solution
+
+
