6e15c10333c21b09fabc2b83e2f58cc576d9d483
hela <48882360+helah20@users.noreply.github.com>
2020-12-23 22:28:28 +0300

    Update shinydashboard.r

--- WEEK3/shinydashboard.r
+++ WEEK3/shinydashboard.r
@@ -1,47 +1,46 @@
 # install.packages("semantic.dashboard")
 # library(semantic.dashboard) to use theme on dashboard 
-
 library(shinydashboard)
 library(shiny)
 # install.packages("DT")
 library(DT)
- # View(iris)
- #View(cars)
 ui <- dashboardPage( skin="blue",
-  dashboardHeader(title = "my dashboard"),
-  
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Iris", tabName = "Iris", icon = icon("tree")),
-      menuItem("Cars", tabName = "cars", icon = icon("car"))
-      
-    )
-  ),
-  ## Body content
-  dashboardBody( 
-    tabItems(
-      tabItem("iris",
-              box(plotOutput("correlation_plot"),width = 8),# any thing we defind it here we must activate it in server
-              box(
-                selectInput("features", "Features:",
-                            c("Sepal.Width", "Petal.Length",
-                              "Petal.Width")),width = 4
-              )
-      ),
-      tabItem("cars",
-              fluidPage(
-              h1("Cars"),
-              dataTableOutput("carstable")
-              )
-     )
-   )
- )
-
+                     dashboardHeader(title = "Cars dashboard"),
+                     
+                     dashboardSidebar(
+                       sidebarMenu(
+                         menuItem("mtcars", tabName = "mtcars", icon = icon("car")),
+                         menuItem("Cars", tabName = "cars", icon = icon("car"))
+                         
+                       )
+                     ),
+                     ## Body content
+                     dashboardBody( 
+                       tabItems(
+                 
+                         tabItem("mtcars",
+                                 box(plotOutput("information"),width = 8),
+                                 box(
+                                   selectInput("features", "Features:",
+                                               c("cyl","hwy","wt")),width = 4
+                                 
+                                 )
+                         ),
+                         tabItem("cars",
+                                 fluidPage(
+                                   h1("Cars"),
+                                   dataTableOutput("carstable")
+                                 )
+                         )
+                       )
+                     )
+                     
 )
 server <- function(input, output) {
-  output$correlation_plot <- renderPlot({
-    plot(iris$Sepal.Length, iris[[input$features]],
-         xlab = "Sepal Length", ylab = "Features") 
+ 
+  output$information<- renderPlot({
+    plot(mtcars$disp,mtcars[[input$features]],
+         xlab = "Displeasment", ylab = "Features")
   })
   output$carstable<- renderDataTable(mtcars)
 }
